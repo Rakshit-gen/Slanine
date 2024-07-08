22b9f99dfd17f066881cef94054b7acc92c20553
Rakshit-gen <64471316+Rakshit-gen@users.noreply.github.com>
2024-07-08 16:29:22 +0530

    Removed the search bar

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -4,17 +4,11 @@ import { UserButton } from '@clerk/nextjs'
 
 const Header = () => {
   return (
-    <div className='p-5 shadow-sm border-b-2 flex justify-between'>
-        <div className='flex gap-2 items-center p-2 border rounded-lg max-w-lg'>
-            <Search />
-            <input type='text' placeholder='Search...'
-            className='outline-none '
-            />
-        </div>
+    <div className='p-5 shadow-sm border-b-2 flex justify-center m-auto items-center'>
         
     <div className='flex gap-4'>
     <UserButton />
-        <h2 className='mt-2 bg-blue-400 p-1 rounded text-xs text-white px-2 hidden md:block lg:block w-fit h-fit'>Join Membership for just $7.99/month
+        <h2 className='mt-0.5 bg-blue-400 p-1 rounded text-xs text-white px-2 hidden md:block lg:block w-fit h-fit'>Join Membership for just $7.99/month
           🔥🔥
         </h2>
     </div>
