2d2b4d04b370b55c97b29e0dbc02c32cd73cfc27
Rakshit-gen <64471316+Rakshit-gen@users.noreply.github.com>
2024-07-08 19:29:45 +0530

    better landing page

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -4,11 +4,11 @@ import { UserButton } from '@clerk/nextjs'
 
 const Header = () => {
   return (
-    <div className='p-5 shadow-sm border-b-2 flex justify-center m-auto items-center'>
+    <div className='p-5 shadow-sm border-b-2 flex justify-center m-auto items-center bg-gray-200'>
         
     <div className='flex gap-4'>
     <UserButton />
-        <h2 className='mt-0.5 bg-blue-400 p-1 rounded text-xs text-white px-2 hidden md:block lg:block w-fit h-fit'>🔥 Join Membership for just $7.99/month 🔥
+        <h2 className='mt-0.5 bg-blue-400 p-1 rounded text-xs text-white px-2 md:block lg:block w-fit h-fit'>🔥 Join Membership for just $7.99/month 🔥
         </h2>
     </div>
     
