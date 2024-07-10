db69d84a06b7ecfc6a937448997f4bf207006d9b
Rakshit-gen <64471316+Rakshit-gen@users.noreply.github.com>
2024-07-10 18:47:28 +0530

    dashboard ui changes

--- app/dashboard/_components/SideNav.tsx
+++ app/dashboard/_components/SideNav.tsx
@@ -41,7 +41,7 @@ const SideNav = () => {
         <div className='mt-5 font-bold'>
             {MenuList.map((menu,index)=>(
                 <Link href={menu.path}>
-                <div className={`flex rounded-2xl gap-2 mb-2 p-3 border-b hover:bg-blue-500 hover:text-white rounded-lg${path===menu.path&&'text-white bg-blue-500'}`}>
+                <div className={`flex rounded-2xl gap-2 mb-2 p-3 border-b hover:bg-blue-500 hover:text-white rounded-lg${path===menu.path&&'m-2 text-white bg-blue-500'}`}>
                     <menu.icons />
                     <h2>{menu.name}</h2>
                 </div>
