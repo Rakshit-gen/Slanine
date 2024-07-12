185727af1c696944692ab34a2151ee90d0463101
Rakshit-gen <64471316+Rakshit-gen@users.noreply.github.com>
2024-07-12 13:47:38 +0530

    Added Logo

--- app/dashboard/_components/SideNav.tsx
+++ app/dashboard/_components/SideNav.tsx
@@ -35,19 +35,21 @@ const SideNav = () => {
     
 
   return (
-    <div className='h-screen p-5 shadow-lg border dark:bg-slate-800 dark:text-white z-20'>
+    <div className='h-screen p-5 shadow-lg border dark:bg-slate-800 dark:text-white'>
         
         <div className='flex justify-center gap-3 pr-3'>
-        <Image src='/logo.svg' alt='logo' width={50} height={50}></Image>
+        <Image src='/logo.svg' alt='logo' width={100} height={100}></Image>
+        {/*
         <h1 className='mt-1 text-3xl dark:text-white'>
-        <span className='text-purple-500 dark:text-purple-300'>S</span>la<span className='text-purple-500 dark:text-purple-300'>ni</span>ne
+        <span className='text-teal-500 dark:text-teal-300'>S</span>la<span className='text-teal-500 dark:text-teal-300'>ni</span>ne
         </h1>
+        */}
         </div>
         
         
         <hr className='my-6' />
 
-        <div className='mt-5 font-bold'>
+        <div className=' font-bold'>
             {MenuList.map((menu,index)=>(
                 <Link href={menu.path}>
                 <div className={`flex rounded-2xl gap-2 mb-2 p-3 border-b hover:bg-blue-500 hover:text-white rounded-lg${path===menu.path&&'m-2 text-white bg-blue-500'}`}>
