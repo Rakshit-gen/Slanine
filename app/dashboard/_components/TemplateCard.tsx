8a99ba71f7394dd712bb1c931bb405c18df989fc
Rakshit-gen <64471316+Rakshit-gen@users.noreply.github.com>
2024-07-14 20:53:29 +0530

    Dashboard Template cards improved

--- app/dashboard/_components/TemplateCard.tsx
+++ app/dashboard/_components/TemplateCard.tsx
@@ -6,12 +6,14 @@ import Link from 'next/link'
 const TemplateCard = (item:TEMPLATE) => {
   return (
     <Link href={'/dashboard/content/'+item?.slug}>
-    <div className='p-10 font-semibold shadow-md rounded-sm border bg-slate-100 flex-col text-center gap-3 cursor-pointer dark:hover:bg-slate-600 transition-smooth hover:shadow-2xl dark:bg-slate-700 hover:bg-slate-300'>
+    <div className='dark:flex bg-gradient-to-r from-slate-800 via-slate-600 to-slate-800'>
+    <div className='p-10 font-semibold shadow-md rounded-sm border bg-slate-100 flex-col text-center gap-3 cursor-pointer dark:hover:bg-slate-600 transition-smooth hover:shadow-2xl dark:bg-transparent hover:bg-slate-300'>
         <div className='items-center justify-center left-[50%] flex m-auto mb-3'>
         <item.icon />
         </div>
         <h2 className='text-lg font-bold'>{item.name}</h2>
         <p>{item.desc}</p>
+    </div>
     </div>
     </Link>
   )
