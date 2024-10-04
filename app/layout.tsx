9a8acd636bf74924bce8559e2b6c3425b3cdd2ad
Shivam <143623476+myselfshivams@users.noreply.github.com>
2024-10-04 10:54:56 +0530

    added:: Meta Tags, Robots.txt, and Sitemap for Improved SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,11 +5,27 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/ThemeProv";
 import { Toaster } from "@/components/ui/toaster";
 
-const inter = Rajdhani({ subsets: ["latin"], weight:['300','400','600','700'] });
+const inter = Rajdhani({ subsets: ["latin"], weight: ['300', '400', '600', '700'] });
+
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://slanine-s4bi.vercel.app/' ;
 
 export const metadata: Metadata = {
   title: "Slanine | Tasks Simplified",
   description: "Generate anything using Gemini AI",
+  
+  openGraph: {
+    title: "Slanine | Tasks Simplified",
+    description: "An AI solution for everything, powered by Gemini 1.5 Flash. Uses templates to do all your tasks.",
+    url: baseUrl,
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Slanine | Tasks Simplified",
+    description: "Generate anything using Gemini AI, powered by Gemini 1.5 Flash.",
+
+  },
 };
 
 export default function RootLayout({
@@ -19,19 +35,20 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-    <html lang="en" suppressHydrationWarning={true}>
-      <body className={inter.className}>
-      <ThemeProvider
+      <html lang="en" suppressHydrationWarning={true}>
+        <body className={inter.className}>
+          <ThemeProvider
             attribute="class"
             defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
-          {children}
-        </ThemeProvider>
-        <Toaster />
+            {children}
+          </ThemeProvider>
+          <Toaster />
         </body>
-    </html>
+      </html>
     </ClerkProvider>
   );
 }
+
