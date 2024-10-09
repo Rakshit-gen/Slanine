85afbea6ef67cdf4508f7c54492379328b0a615f
Rakshit-gen <64471316+Rakshit-gen@users.noreply.github.com>
2024-10-09 14:18:03 +0530

    Added several features

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,9 @@
-import { SignIn } from '@clerk/nextjs'
+import { SignIn, useAuth } from '@clerk/nextjs'
+import { useRouter } from 'next/router'
 import React from 'react'
 
 const page = () => {
+  
   return (
     <div className='flex items-center h-screen justify-center self-center'>
     <SignIn />
--- middleware.ts
+++ middleware.ts
@@ -1,41 +1,34 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-import { ratelimit } from "./lib/ratelimit";
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { ratelimit } from './lib/ratelimit';
 
-const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
-
-const isHomeRoute = createRouteMatcher(["/"]);
+const isProtectedRoute = createRouteMatcher([
+    '/dashboard(.*)',
+]);
 
 async function ratelimitMiddleware(request: NextRequest) {
-  const ip = request.ip ?? request.headers.get("x-forwarded-for") ?? "unknown";
-
+  const ip = request.ip ?? request.headers.get('x-forwarded-for') ?? 'unknown';
+  
   const { success, limit, reset, remaining } = await ratelimit.limit(
     `ratelimit_middleware_${ip}`
   );
-
+  
   if (!success) {
-    return new NextResponse("Too Many Requests", {
+    return new NextResponse('Too Many Requests', {
       status: 429,
       headers: {
-        "X-RateLimit-Limit": limit.toString(),
-        "X-RateLimit-Remaining": remaining.toString(),
-        "X-RateLimit-Reset": reset.toString(),
+        'X-RateLimit-Limit': limit.toString(),
+        'X-RateLimit-Remaining': remaining.toString(),
+        'X-RateLimit-Reset': reset.toString(),
       },
     });
   }
-
+  
   return null;
 }
 
 export default clerkMiddleware(async (auth, req) => {
-  const { userId } = await auth(); // Get userId from the auth method
-
-  // If user is logged in and the request is to the '/' route, redirect to '/dashboard'
-  if (userId && isHomeRoute(req)) {
-    return NextResponse.redirect(new URL("/dashboard", req.url));
-  }
-
   if (isProtectedRoute(req)) {
     await auth().protect();
   }
